040916d413848c90a58c0881bfaefdcda2d83299
kyawnaingphyo765-maker <kyawnaingphyo765@gmail.com>
2026-09-08 01:20:48 +0630

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, send_file
 
-app = Flask(name)
+app = Flask(__name__)
 
 @app.route("/")
 def home():
     return send_file("index.html")
 
-if name == "main":
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
